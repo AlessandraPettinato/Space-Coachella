bf87110683a07332cba37ba311e5e11ff6b35010
Isabel <isabel.mehlmann@gmail.com>
2021-05-11 17:17:07 +0200

    added about us component with pics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from "./components/Header/Header";
 import "./App.css";
+import About from "./components/About";
+
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<About />
 		</div>
 	);
 }
