207e54052e7819037dc5ae59e6a37c7987b1ead0
Alessandra Pettinato <alessandrapettinato@Alessandras-MacBook-Pro.local>
2021-05-11 14:16:31 +0200

    Add header components and media queries. Style navbar accordingly. Clean code

---
+++ src/components/Header/MenuItems.js
@@ -0,0 +1,12 @@
+export const MenuItems = [
+	{
+		title: "So far...",
+		url: "/sofar",
+		cName: "nav-links",
+	},
+	{
+		title: "...coming up next",
+		url: "/comingup",
+		cName: "nav-links",
+	},
+];
