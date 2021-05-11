09ab1f14153ceeb0c04adaccf783b5f49daef54f
Alessandra Pettinato <alessandrapettinato@Alessandras-MacBook-Pro.local>
2021-05-11 17:46:36 +0200

    Add landing page. Style accordingly (mobile and desktop)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Header";
+import LandingPage from "./components/LandingPage/LandingPage";
 import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<LandingPage />
 		</div>
 	);
 }
