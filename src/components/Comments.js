42400676bac1564830abb007f60ffd209ebdd150
anca2196 <62710917+anca2196@users.noreply.github.com>
2021-05-12 12:30:58 +0300

    Update Comments.js

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -19,21 +19,21 @@ export default function Comments() {
 			<div className="comment-section">
 				<div className="comment-card">
 					<h5>
-						I spent the whole night replicating the dance of Britney Spears' Ops
-						I Did It Again. It was amazing! #freebritney
+						I spent the whole night replicating the dance of Britney Spears' 'Ops
+						I Did It Again'. It was amazing! #freebritney
 					</h5>
 					<h3>- Melinda</h3>
 				</div>
 				<div className="comment-card">
 					<h5>
 						You'll have a transformative experience for the lower price of 3
-						mars rocks! Can't wait to be here again enxt year!
+						mars rocks! Can't wait to be here again next year!
 					</h5>
 					<h3>- Bill</h3>
 				</div>
 				<div className="comment-card">
 					<h5>
-						I got very wasted. I was very difficult going back to my orbit, but
+						I got very wasted. It was very difficult going back to my orbit, but
 						I loved it! Mars rulez.
 					</h5>
 
