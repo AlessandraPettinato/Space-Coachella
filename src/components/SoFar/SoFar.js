584dd894b38000050beea2bf717c7854af0c6618
Anca <anca96laura21@yahoo.com>
2021-06-10 21:56:15 +0300

    cleanup

--- src/components/SoFar/SoFar.js
+++ src/components/SoFar/SoFar.js
@@ -1,11 +1,13 @@
 import ImageSlider from "./ImageSlider";
 import Comments from "./Comments";
+import "./SoFar.css"
 
 const SoFar = ({ partyPhotos }) => {
 	return (
 		<div className="soFar-container">
 			<div>
 				<div className="embed-player">
+					<h1 className="so-far-title1"> <p className="candy-text">Take a look at </p> <p className="pink-text">our </p> <p className="green-text"> alien </p>  <p className="blue-text">playlist...</p></h1>
 					<iframe
 						src="https://open.spotify.com/embed/playlist/2sjNT7udCWklujkW3ipMj0"
 						width="300"
@@ -18,12 +20,16 @@ const SoFar = ({ partyPhotos }) => {
 			</div>
 			<div>
 				<div className="main-caption">
+				
 					<h1>
+					<p className="candy-text">...and checkout </p>
+					<br></br>
 						<span className="pink">P</span>
 						<span className="blue">e</span>
 						<span className="yellow">r</span>
 						<span className="green">c</span>
-						<span className="candy">y</span>
+						<span className="candy">y</span> '
+						<span className="blue">s</span>
 
 						<br classaName="hide" />
 						<span className="break">I</span>
@@ -35,6 +41,7 @@ const SoFar = ({ partyPhotos }) => {
 						<span className="candy">r</span>
 						<span className="green">a</span>
 						<span className="candy">m</span>
+						<span className="blue">!</span>
 					</h1>
 				</div>
 				<ImageSlider partyPhotos={partyPhotos} />
